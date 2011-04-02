c0732f112ded76b380866b82b6dad4cff1fcd260
Norton Allen <allen@huarp.harvard.edu>
2011-04-02 15:50:48 +0000

    Changes for flight

--- TM/Hal.sol
+++ TM/Hal.sol
@@ -48,8 +48,8 @@ Command_Set = 'A'
 ; TM name       : NOH1Set NOH2Set ClONO2Set NO2FlSet
 
 
-dtoa  NOxH1F  0xCEE   {Z:0 A:1966 B:1147 C:462  D:377 E:340}
-dtoa  NOxH2F  0xD60   {Z:0 A:1966 B:1147 C:737  D:533 E:340}
+dtoa  NOxH1F  0xCEE   {Z:0 A:431  B:401  C:358  D:351 E:340}
+dtoa  NOxH2F  0xD60   {Z:0 A:1134 B:737	 C:533  D:455 E:340}
 dtoa  CNxHF   0xCEC   {Z:0 A:2047 B:1639 C:1229 D:819 E:409}
 dtoa  NO2xNF  0xC60   {Z:0 A:2047 B:1639 C:1024 D:819 E:409}
 
