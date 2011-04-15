ee6a8426df2d2763b54f29388b8749fabae16ded
Norton Allen <allen@huarp.harvard.edu>
2011-04-15 18:49:57 +0000

    Cleaned up a lot of plots

--- eng/ghalt.m
+++ eng/ghalt.m
@@ -1,5 +1,6 @@
 function fig = ghalt(varargin);
 % ghalt(...)
 % Temps
-ffig = ne_group(varargin,'Temps','phalta','phalte','phaltsfb','phaltsff','phaltsf2b','phaltsf2f');
+ffig = ne_group(varargin,'Temps','phaltsfb','phaltsff','phaltsf2b','phaltsf2f');
 if nargout > 0 fig = ffig; end
+% Removed ,'phalta','phalte' Air*T and Exh*T
