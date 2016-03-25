f47d0e7b14edd5b627052ac2565e4b2519bdf261
Norton Allen <allen@huarp.harvard.edu>
2016-03-25 13:53:28 +0000

    Numerous changes

--- eng/ghall.m
+++ eng/ghall.m
@@ -1,6 +1,5 @@
 function fig = ghall(varargin);
 % ghall(...)
-% Lamps PT
-ffig = ne_group(varargin,'Lamps PT','phalldt','phalllmt','phallp');
-% ffig = ne_group(varargin,'Lamps','phalldt','phallir','phallirs','phalllmt','phallp','phallrfbw','phallrffw','phallrfs','phalls');
+% Lamps
+ffig = ne_group(varargin,'Lamps','phalldt','phallir','phallirs','phalllmt','phallp','phallrfbw','phallrffw','phallrfs','phalls');
 if nargout > 0 fig = ffig; end
