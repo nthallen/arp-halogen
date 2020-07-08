162ebe975232edf52064aeca8ee6721ff9bb3217
Norton Allen <allen@huarp.harvard.edu>
2020-07-08 14:29:51 -0500

    Compiled and cleaned up eng directory

--- eng/ghall.m
+++ eng/ghall.m
@@ -1,5 +1,5 @@
 function fig = ghall(varargin);
 % ghall(...)
 % Lamps
-ffig = ne_group(varargin,'Lamps','phalldt','phallir','phallirs','phalllmt','phallp','phallrfbw','phallrffw','phallrfs','phalls');
+ffig = ne_group(varargin,'Lamps','phalldt','phallir','phallirs','phalllmt','phallp','phallrfbw','phallrffw','phalls');
 if nargout > 0 fig = ffig; end
--- eng/ghalrf.m
+++
@@ -1,5 +0,0 @@
-function fig = ghalrf(varargin);
-% ghalrf(...)
-% RF
-ffig = ne_group(varargin,'RF','phalrfa','phalrfs','phalrft');
-if nargout > 0 fig = ffig; end
