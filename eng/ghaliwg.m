daa3bf4827a45f8b8ac2ed904ca3cf332969fd9a
Norton Allen <allen@huarp.harvard.edu>
2021-06-13 00:26:45 -0400

    6.1.4 Compiled

---
+++ eng/ghaliwg.m
@@ -0,0 +1,5 @@
+function fig = ghaliwg(varargin);
+% ghaliwg(...)
+% IWG1
+ffig = ne_group(varargin,'IWG1','phaliwgl','phaliwglon','phaliwgt','phaliwgp','phaliwgs','phaliwgm','phaliwga');
+if nargout > 0 fig = ffig; end
---
+++ eng/ghaliwg1_stat.m
@@ -0,0 +1,5 @@
+function fig = ghaliwg1_stat(varargin);
+% ghaliwg1_stat(...)
+% IWG1 Stat
+ffig = ne_group(varargin,'IWG1 Stat','phaliwg1_statcp','phaliwg1_stattd','phaliwg1_stats');
+if nargout > 0 fig = ffig; end
