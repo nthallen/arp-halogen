f3c925fb33ae8c66e437514a63d8b9cdc3be025b
Norton Allen <allen@huarp.harvard.edu>
2021-08-08 15:31:02 -0500

    Suppress TRU/BMM commands if subbusd_slcan is not loaded

--- TM/bmm.cmd
+++ TM/bmm.cmd
@@ -3,21 +3,34 @@
 
   #ifdef SERVER
     subbuspp *CAN;
+    bool bmm_is_present = false;
     
     void CAN_init() {
       CAN = new subbuspp("CAN");
       CAN->load();
+      int subfunc = CAN->load();
+      if (subfunc == 0) {
+        msg(2, "subbusd_slcan Not Found, no TRU functions");
+      } else {
+        bmm_is_present = true;
+      }
     }
   #endif
 %}
 
 &command
- : BMM &BMM_ID &BMM_Switch &BMM_On_Off * {
-     CAN->write_ack(($2<<8)+0x30, $3+$4);
-   }
- : TRU &BMM_On_Off * {
-     /* Hard coded to ID==1, commands 5,4 */
-     CAN->write_ack((1<<8)+0x30, 5-$2);
+  : BMM &BMM_ID &BMM_Switch &BMM_On_Off * {
+      if (bmm_is_present)
+        CAN->write_ack(($2<<8)+0x30, $3+$4);
+      else
+        msg(2, "TRU/BMM functions disabled");
+    }
+  : TRU &BMM_On_Off * {
+      /* Hard coded to ID==1, commands 5,4 */
+      if (bmm_is_present)
+        CAN->write_ack((1<<8)+0x30, 5-$2);
+      else
+        msg(2, "TRU/BMM functions disabled");
    }
  ;
 &BMM_ID <int>
